3f3bb11661aab3555365a7d3697c7a525d728a42
Charitha NL <charithanl508@gmail.com>
2026-01-02 14:23:03 +0530

    understanding the dataset

--- code.py
+++ code.py
@@ -1,7 +1,15 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # give you file path here
 dataset = pd.read_csv('Telco-Customer-Churn.csv')
 
 dataset.head()
+
+# Understanding the Dataset
+print(dataset.isnull().sum())
+print(dataset.describe())
+
